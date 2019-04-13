1b3d46adaa02ff59c967580e62d1a9b486b91a82
Reginaldo <reginaldolribeiro@gmail.com>
2019-04-13 18:57:08 -0300

    Fecth Github API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: "repo-1" },
-    { id: 2, name: "repo-2" },
-    { id: 3, name: "repo-3" }
-  ]);
+  const [repositories, setRepositories] = useState([]);
 
-  function handleAddRepository() {
-    setRepositories([
-      ...repositories,
-      { id: Math.random(), name: "Novo repo" }
-    ]);
-    console.log("handle add repository");
+  useEffect(async () => {
+    const response = await fetch(
+      "https://api.github.com/users/reginaldolribeiro/repos"
+    );
+    const data = await response.json();
+
+    setRepositories(data);
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `Voce tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
   }
 
   return (
-    <>
-      <ul>
-        {repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
-      <button onClick={handleAddRepository}>Adicionar repositorio</button>
-    </>
+    <ul>
+      {repositories.map(repo => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
+      ))}
+    </ul>
   );
 }
